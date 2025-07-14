b6c2f6a622fd7b8d6bf2ac550fed62e877c1a849
tanya-yanko <yanko200662@gmail.com>
2025-07-14 20:29:05 +0300

    fixed3

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -2,40 +2,38 @@
 import { useQuery } from '@tanstack/react-query';
 import { fetchNoteById } from '@/lib/api';
 import css from './NoteDetails.module.css';
+import { useParams } from "next/navigation";
 
-interface NoteDetailsClientProps {
-  noteId: number;
-}
+const NoteDetailClient = () => {
+  const { id } = useParams<{ id: string }>();
+  const idNum = Number(id);
 
-export default function NoteDetailsClient({ noteId }: NoteDetailsClientProps) {
-  const isValidId = !isNaN(noteId) && Number.isInteger(noteId) && noteId > 0;
-
-  if (!isValidId) {
-    return <p>Invalid note ID.</p>;
-  }
-
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const { data, isLoading, error } = useQuery({
-    queryKey: ['note', noteId],
-    queryFn: () => fetchNoteById(noteId),
+  const {
+    data: note,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["note", id],
+    queryFn: () => fetchNoteById(idNum),
     refetchOnMount: false,
   });
 
   if (isLoading) return <p>Loading, please wait...</p>;
-  if (error || !data) return <p>Something went wrong.</p>;
 
-  const { title, content, createdAt } = data;
+  if (error || !note) return <p>Something went wrong.</p>;
 
   return (
     <div className={css.container}>
       <div className={css.item}>
         <div className={css.header}>
-          <h2>{title}</h2>
+          <h2>{note.title}</h2>
           <button className={css.editBtn}>Edit note</button>
         </div>
-        <p className={css.content}>{content}</p>
-        <p className={css.date}>{createdAt}</p>
+        <p className={css.content}>{note.content}</p>
+        <p className={css.date}>{note.createdAt}</p>
       </div>
     </div>
   );
-}
+};
+
+export default NoteDetailClient;
--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,28 +1,25 @@
 import { HydrationBoundary, dehydrate, QueryClient } from '@tanstack/react-query';
 import NoteDetailsClient from './NoteDetails.client';
 import { fetchNoteById } from '@/lib/api';
-import type { ReactElement } from 'react';
 
-interface NoteDetailsProps {
-  params: { id: string };
+
+interface NoteDetailPageProps {
+  params: Promise<{ id: string }>;
 }
 
-export default async function Page({ params }: NoteDetailsProps): Promise<ReactElement> {
-  const noteId = Number(params.id);
-
+export default async function NoteDetailPage({ params }: NoteDetailPageProps) {
+  const { id } = await params;
+  const idNum = Number(id);
   const queryClient = new QueryClient();
 
   await queryClient.prefetchQuery({
-    queryKey: ['note', noteId],
-    queryFn: () => fetchNoteById(noteId),
+    queryKey: ["note", id],
+    queryFn: () => fetchNoteById(idNum),
   });
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <NoteDetailsClient noteId={noteId} />
+      <NoteDetailsClient />
     </HydrationBoundary>
   );
 }
-
-
-
